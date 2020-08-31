eabaad72f840b7b3d5aac03f76268403ed25e261
CharlesJV <charles764jaison@gmail.com>
2020-08-30 22:04:52 -0400

    Final commit

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 export default {
   name: "AddTodo",
   data() {
@@ -22,12 +22,15 @@ export default {
     };
   },
   methods: {
-    addTodo() {
+    addTodo(e) {
+      e.preventDefault();
       const newTodo = {
-        id: uuid.v4(),
+        id: uuidv4(),
         title: this.title,
         completed: false,
       };
+      this.$emit("add-todo", newTodo);
+      this.title = "";
     },
   },
 };
